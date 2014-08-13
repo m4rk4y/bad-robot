813507197cb511eef3e122191ff7d3348bfcff13
m4rk4y <m4rk4y@users.noreply.github.com>
2014-08-14 09:41:18 +1000

    Test from stdin too

--- run_tests.bat
+++ run_tests.bat
@@ -7,7 +7,7 @@ setlocal enabledelayedexpansion
 
 call :testIt test_input1.txt test_output1.txt
 call :testIt test_input2.txt test_output2.txt
-
+call :testItFromStdin test_input1.txt test_output1.txt
 goto :eof
 
 :testIt
@@ -23,5 +23,19 @@ if %diffCount% gtr 0 (
 ) else (
     echo OK: test %in% succeeded
 )
+goto :eof
 
-
+:testItFromStdin
+set in=%1
+set out=%2
+( bad_robot < %in% 2>&1 ) > out.txt
+REM diff -q counts different line-endings. We don't care about those, so
+REM use diff ... | wc -l
+for /f %%t in ( 'diff out.txt %out% ^| wc -l' ) do set /a diffCount=%%t
+if %diffCount% gtr 0 (
+    echo ERROR: stdin test %in% failed:
+    diff -c out.txt %out%
+) else (
+    echo OK: stdin test %in% succeeded
+)
+goto :eof
